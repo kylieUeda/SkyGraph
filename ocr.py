6a0832a91b054732d38e7873b71396d636b45621
Kylie (Kanade) Ueda <uedak@etown.edu>
2026-04-17 12:12:22 -0400

    Added Error Handling

--- ocr.py
+++ ocr.py
@@ -12,6 +12,10 @@ def extract_flight_info(image_path):
     
     # read texts from an image
     results = reader.readtext(image_path, detail=0)
+
+    # Handling errors when it has no texts
+    if not results:
+        raise ValueError("OCR detected no text")
     
     full_text = " ".join(results).upper()
 
@@ -23,6 +27,11 @@ def extract_flight_info(image_path):
         for dep, dest in codes_set
         if dep in IATA and dest in IATA
     ]
+
+    # Handling errors in case any pairs of codes are not found
+    if not valid_sets:
+        raise ValueError("Not valid IATA code set found.")
+
     
     # remove duplicate
     u_codes = list(set(valid_sets))
@@ -32,6 +41,10 @@ def extract_flight_info(image_path):
 # Get other info from the dataset
 def get_airport_info(code):
     r = iata_df[iata_df["iata_code"] == code]
+
+    # Handling the case when the dataset has no info
+    if r.empty:
+        return { "iata_code" : code, "name" : "Unknown", "latitude" : None, "longitude" : None }
 
     return {
         "iata_code" : code,
